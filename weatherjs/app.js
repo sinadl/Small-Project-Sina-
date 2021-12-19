7358558c222dcb942d83cd055ff8483a5208e11c
sina <sinadsina78@gmail.com>
2021-12-20 01:04:01 +0400

    weather js
    set local storage for saving city and countries

--- weatherjs/app.js
+++ weatherjs/app.js
@@ -1,14 +1,26 @@
-const weather = new Weather('Boston','US');
 const ui = new UI();
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+const weather = new Weather( weatherLocation.city , weatherLocation.country);
 
 document.addEventListener('DOMContentLoaded', getWeather);
 
-
+document.getElementById('w-change-btn').addEventListener('click', (e) =>{
+    const city = document.getElementById('city').value;
+    const country = document.getElementById('state').value;
+    weather.changeLocation(city,country);
+    getWeather();
+    $('#locModal').modal('hide');
+    storage.setLocationData(city,country);
+});
 function getWeather(){
     weather.getWeather()
         .then(result => {
             console.log(result);
             ui.paint(result);
+        if(result.responsePost.cod == '404'){
+                console.log("city not found!!!")
+        }
         })
         .catch(err => console.log(err))
 }
--- weatherjs/ui.js
+++ weatherjs/ui.js
@@ -12,13 +12,15 @@ class UI{
     }
     paint(weather){
         this.location.textContent = `${weather.responsePost.sys.country}  -  ${weather.responsePost.name}`;
-        this.desc.textContent = `${weather.responsePost.weather[0].main} -  ${weather.responsePost.weather[1].main}`;
+        this.desc.textContent = `${weather.responsePost.weather[0].main}`;
         this.string.innerHTML = `<strong>${weather.responsePost.main.temp} F</strong><span class="temp"><p>Max:${weather.responsePost.main.temp_max} F</p><p>Min:${weather.responsePost.main.temp_min} F</p></span>`;
         this.icon.setAttribute("src",`http://openweathermap.org/img/wn/${weather.responsePost.weather[0].icon}@2x.png`);
         this.feelslike.textContent = `feelslike: ${weather.responsePost.main.feels_like} F`;
         this.humidity.textContent = `Humidity: ${weather.responsePost.main.humidity}`;
         this.clouds.textContent = `Clouds: ${weather.responsePost.clouds.all}%`;
         this.wind.innerHTML = `wind: <strong>speed:</strong>${weather.responsePost.wind.speed} -  <strong>deg:</strong>${weather.responsePost.wind.deg} - <strong>gust:</strong>${weather.responsePost.wind.gust}`;
-        document.querySelector(".main-bg").classList.add(`${weather.responsePost.weather[0].main}`);
+        const oldweather = weather.responsePost.weather[0].main;
+        document.querySelector(".main-bg").classList.remove(oldweather);
+        document.querySelector(".main-bg").classList.add(`${oldweather}`);
     }
 }
